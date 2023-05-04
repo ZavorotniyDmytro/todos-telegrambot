fd6f8a77cd945c7e1f4b71f6a995d83af095314e
Zavorotniy Dmytro <121565594+ZavorotniyDmytro@users.noreply.github.com>
2023-05-04 19:10:19 +0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,26 +5,26 @@ import { TelebotModule } from './telebot/telebot.module';
 import { DatabaseModule } from './database/database.module';
 
 @Module({
-	imports: [
-		ConfigModule.forRoot({			
-			envFilePath: ['.env',],			
-			isGlobal: true,
-			validationSchema: Joi.object({
-				PORT: Joi.number().required().default(4999),
-				POSTGRES_HOST: Joi.string().required(),
-				POSTGRES_USER: Joi.string().required(),
-				POSTGRES_DB: Joi.string().required(),
-				POSTGRES_PASSWORD: Joi.string().required(),
-				POSTGRES_PORT: Joi.number().required().default(5432),
-				TELEGRAM_BOT_TOKEN: Joi.string().required(),
-				OPENWEATHER_API_KEY: Joi.string().required(),
-				OPENWEATHERMAP_BASE_URL: Joi.string().required(),
-			})
-		}),
-		TelebotModule,
-		DatabaseModule,
-	],
-	controllers: [],
-	providers: [ConfigService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env'],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number().required().default(4999),
+        POSTGRES_HOST: Joi.string().required(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_DB: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_PORT: Joi.number().required().default(5432),
+        TELEGRAM_BOT_TOKEN: Joi.string().required(),
+        OPENWEATHER_API_KEY: Joi.string().required(),
+        OPENWEATHERMAP_BASE_URL: Joi.string().required(),
+      }),
+    }),
+    TelebotModule,
+    DatabaseModule,
+  ],
+  controllers: [],
+  providers: [ConfigService],
 })
 export class AppModule {}
